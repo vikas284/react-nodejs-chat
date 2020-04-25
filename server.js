024dfb34097ec46a0e5fddff9a37b90e7d6fbce4
vikasgarg284 <vgarg2@cisco.com>
2020-04-25 22:21:24 +0530

    Server code update

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express = require('express')
 const app = express();
 var path = require('path');
 app.set('port',(process.env.prt || 5000))
-app.use(express.static(path.join(__dirname, '/client/build')))
+app.use(express.static(path.resolve(__dirname, './client/build')));
 
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname,'/client/build', 'index'));
+    res.sendFile(path.join(__dirname,'./client/build/index.html'));
 });
 app.get('/users', (req, res) => {
     res.json([{ email: "vish1u@gmail.com", password: "Vishu29@" }]);
