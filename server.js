fba3f9843f7e20f0de8591a66c2ba39e16c82246
vikasgarg284 <vgarg2@cisco.com>
2020-04-25 21:10:39 +0530

    Server Port Update

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ app.use(express.static(path.join(__dirname, '/client/build')))
 
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname,'/client/build', '/index.html'));
+    res.sendFile(path.join(__dirname,'/client/build', 'index.html'));
 });
 app.get('/users', (req, res) => {
     res.json([{ email: "vish1u@gmail.com", password: "Vishu29@" }]);
 })
 
 server = app.listen(5000,function(){
-    console.log("Server is listening on port number :5000")
+    console.log("Server is listening on port num  :5000")
 })
