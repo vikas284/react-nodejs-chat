9e2ddf5527c1e1fefefc9c2c79ccbbfb150e61b5
vikasgarg284 <vgarg2@cisco.com>
2020-04-26 14:22:32 +0530

    Server code update

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ app.use(express.static(path.resolve(__dirname, 'client/build')));
 
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname,'client/build/index.html'));
+    res.sendFile(path.join(__dirname,'index.html'));
 });
 app.get('/users', (req, res) => {
     res.json([{ email: "vish1u@gmail.com", password: "Vishu29@" }]);
 })
 
 server = app.listen(port,function(){
-    console.log(`Server is  listening on port num 12389  :${port}`)
+    console.log(`Server is  listening on port num 12390  :${port}`)
 })
