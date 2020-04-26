d3f87cb5741eab1f2153c4153583c5172d82ee58
vikasgarg284 <vgarg2@cisco.com>
2020-04-26 14:09:01 +0530

    Build folder added

--- server.js
+++ server.js
@@ -2,16 +2,16 @@ const express = require('express')
 const app = express();
 var path = require('path');
 var port = process.env.PORT || 3000;
-app.use(express.static(path.resolve(__dirname, 'build')));
+app.use(express.static(path.resolve(__dirname, 'client/build')));
 
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname,'build/index.html'));
+    res.sendFile(path.join(__dirname,'client/build/index.html'));
 });
 app.get('/users', (req, res) => {
     res.json([{ email: "vish1u@gmail.com", password: "Vishu29@" }]);
 })
 
 server = app.listen(port,function(){
-    console.log(`Server is  listening on port num 123  :${port}`)
+    console.log(`Server is  listening on port num 12389  :${port}`)
 })
